5d9296ab23decdfdd53bb65d3dc4050286a9afeb
L0laapk3 <L0laapk3@gmail.com>
2020-12-27 17:01:16 +0100

    Cleanup config and info for gui

--- config.py
+++ config.py
@@ -1,18 +1,19 @@
-
-
-# If true, the laser will only be on while boosting.
+# If true, the laser will only be on while boosting. Otherwise the lasers will always be enabled.
 DURING_BOOST_ONLY = False
 
 
-# if true, lasers are enabled by the twitch chat. Otherwise, the lasers are always available for everyone.
+# TWITCH CHAT INTERACTION OPTIONS
 TWITCH_CHAT_INTERACTION = False
 LASER_DURATION = 5
 ALLOW_MULTIPLE_AT_ONCE = True
 
 
+# Controls bouncing of lasers. Be warned, bounces can make lasers unpredictable and laggy.
+BOUNCE_SEGMENTS = 1
+
+
 # You shouldn't need to change these
 BASE_PUSH_STRENGTH = 9001
-BOUNCE_SEGMENTS = 3
 
 LASERLINES = 50
 SCATTERLINES = 30
